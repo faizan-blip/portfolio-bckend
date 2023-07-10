3918e39275080577feb5653f6ae18c41f6b35475
faizan-blip <faizanrock753@gmail.com>
2023-07-10 23:02:32 +0530

    vercel added

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const dbConnect = require('./config/database')
 require('dotenv').config()
 
-const PORT = process.env.PORT || 5000
+// const PORT = process.env.PORT || 5000
 app.use(cors({
     origin: 'https://faizanak-portfolio.netlify.app',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
@@ -16,9 +16,9 @@ app.use(express.json())
 const routes = require('./routes/files')
 app.use('/api/contact' , routes)
 dbConnect();
-app.listen(PORT , ()=>{
-    console.log("Port is running successfully");
-})
+// app.listen(PORT , ()=>{
+//     console.log("Port is running successfully");
+// })
 
 app.get('/' , (req,res)=>{
     res.send("Running contact")
