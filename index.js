033144f2f0a7942f539fc03d654e9676b226e6e4
faizan-blip <faizanrock753@gmail.com>
2023-07-10 23:37:10 +0530

    index

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 const cors = require('cors');
-const dbConnect = require('./config/database')
-require('dotenv').config()
+const dbConnect = require('./config/database');
+require('dotenv').config();
 
-const PORT = process.env.PORT || 5000
-app.use(function (req, res, next) {
-    // Enabling CORS
-    res.header("Access-Control-Allow-Origin", "https://faizanak-portfolio.netlify.app");
-    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
-    next();
-  });
-  
+const PORT = process.env.PORT || 5000;
 
-app.use(express.json())
+app.use(cors({
+  origin: 'https://faizanak-portfolio.netlify.app',
+  methods: ['GET', 'HEAD', 'OPTIONS', 'POST', 'PUT'],
+  allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept', 'x-client-key', 'x-client-token', 'x-client-secret', 'Authorization']
+}));
 
-const routes = require('./routes/files')
-app.use('/api/contact' , routes)
+app.use(express.json());
+
+const routes = require('./routes/files');
+app.use('/api/contact', routes);
+
 dbConnect();
-app.listen(PORT , ()=>{
-    console.log("Port is running successfully");
-})
 
-app.get('/' , (req,res)=>{
-    res.send("Running contact")
-})
+app.listen(PORT, () => {
+  console.log("Port is running successfully");
+});
+
+app.get('/', (req, res) => {
+  res.send("Running contact");
+});
