91c257ba608ed937db83a77aea41f3eddfb14e8f
faizan-blip <faizanrock753@gmail.com>
2023-07-10 23:29:57 +0530

    index

--- index.js
+++ index.js
@@ -5,12 +5,14 @@ const dbConnect = require('./config/database')
 require('dotenv').config()
 
 const PORT = process.env.PORT || 5000
-app.use(cors({
-    origin: '*',
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization', 'phone'],
-  }));
-  
+app.use(function (req, res, next) {
+    //Enabling CORS
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
+      next();
+    });
+
 app.use(express.json())
 
 const routes = require('./routes/files')
