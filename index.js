fb8f112a3922a926b376062eebe3f173f4535698
faizan-blip <faizanrock753@gmail.com>
2023-07-10 23:31:20 +0530

    index

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ require('dotenv').config()
 const PORT = process.env.PORT || 5000
 app.use(function (req, res, next) {
     //Enabling CORS
-    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Origin", "https://faizanak-portfolio.netlify.app/");
     res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
       next();
